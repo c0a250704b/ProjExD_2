387f30f10786fa9ded4574021ee27d7e6a9dcc8d
kadomoto <c0a250704b@edu.teu.ac.jp>
2026-04-21 16:29:54 +0900

    追加機能2

--- dodge_bomb.py
+++ dodge_bomb.py
@@ -46,6 +46,21 @@ def gameover(screen: pg.Surface) -> None:
     pg.display.update()
     time.sleep(5)
 
+def init_bb_imgs() -> tuple[list[pg.Surface], list[int]]:
+    """
+    十段階の大きさの違う爆弾Surfaceと加速度のリストを作成する関数
+    引数：なし
+    戻り値：爆弾Surfaceのリスト、加速度のリスト
+    """
+    bb_imgs = []
+    for r in range(1, 11):
+        bb_img = pg.Surface((20*r, 20*r))
+        pg.draw.circle(bb_img, (255, 0, 0), (10*r, 10*r), 10*r)
+        bb_img.set_colorkey((0, 0, 0)) #  黒色の透過
+        bb_imgs.append(bb_img)
+    bb_accs = [a for a in range(1, 11)]
+    return bb_imgs, bb_accs
+
 def main():
     pg.display.set_caption("逃げろ！こうかとん")
     screen = pg.display.set_mode((WIDTH, HEIGHT))
@@ -61,6 +76,8 @@ def main():
     bb_rct.centerx = random.randint(0, WIDTH)
     bb_rct.centery = random.randint(0, HEIGHT)
     vx, vy = +5, +5
+
+    bb_imgs, bb_accs = init_bb_imgs()
 
     clock = pg.time.Clock()
     tmr = 0
@@ -95,7 +112,16 @@ def main():
             kk_rct.move_ip(-sum_mv[0], -sum_mv[1])
         
         screen.blit(kk_img, kk_rct)
-        bb_rct.move_ip(vx, vy)
+
+        avx = vx*bb_accs[min(tmr//500, 9)]
+        avy = vy*bb_accs[min(tmr//500, 9)]
+        bb_img = bb_imgs[min(tmr//500, 9)]
+
+        bb_rct.width = bb_img.get_rect().width #  Surfaceの大きさに合わせたwidth, heightの更新
+        bb_rct.height = bb_img.get_rect().height
+
+        bb_rct.move_ip(avx, avy)
+
         yoko, tate = check_bound(bb_rct)
         if not yoko:
             vx *= -1
